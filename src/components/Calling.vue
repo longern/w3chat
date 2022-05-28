f98a7ec2f03c953ed98c8ed237b1290f92196fa7
longern <771815005@qq.com>
2022-05-28 18:14:33 +0800

    Add resize button

--- src/components/Calling.vue
+++ src/components/Calling.vue
@@ -1,33 +1,34 @@
 <script setup>
+import { ref } from "vue";
 import stream, { myself, incomings } from "@/composables/stream";
 
 defineProps({ modelValue: Boolean });
 const emit = defineEmits(["update:modelValue"]);
 
+const small = ref(false);
+
 function hangup() {
   stream.stop();
   for (const incoming of incomings.value)
-    if (incoming.connection)
-      incoming.connection.close();
+    if (incoming.connection) incoming.connection.close();
   incomings.value = [];
-  emit('update:modelValue', false);
+  emit("update:modelValue", false);
 }
 
 stream.on("remove", () => {
   if (!myself.value || !incomings.value.length)
-    emit('update:modelValue', false);
+    emit("update:modelValue", false);
 });
 </script>
 
 <template>
-  <div v-if="modelValue" class="fullscreen-modal">
+  <div
+    v-if="modelValue"
+    :class="small ? 'smallscreen-modal' : 'fullscreen-modal'"
+    @click="small = false"
+  >
     <template v-if="myself">
-      <video
-        muted
-        autoplay
-        :srcObject.prop="myself"
-        class="position-absolute object-fit-cover"
-      ></video>
+      <video muted autoplay :srcObject.prop="myself"></video>
     </template>
     <template v-if="incomings.length">
       <video
@@ -35,21 +36,54 @@ stream.on("remove", () => {
         :key="st.id"
         autoplay
         :srcObject.prop="st"
-        class="incomings position-absolute object-fit-cover"
+        class="incomings"
       ></video>
     </template>
-    <div class="calling-buttons">
+    <div v-if="!small" class="calling-buttons">
+      <button class="btn-icon">
+        <span class="mdi mdi-resize" @click.stop="small = !small"></span>
+      </button>
       <button class="btn-icon hangup">
-        <span
-          class="mdi mdi-phone-hangup"
-          @click="hangup"
-        ></span>
+        <span class="mdi mdi-phone-hangup" @click="hangup"></span>
       </button>
     </div>
   </div>
 </template>
 
 <style>
+.smallscreen-modal {
+  position: absolute;
+  top: 5%;
+  right: 5%;
+  z-index: 100;
+  overflow: hidden;
+}
+
+.smallscreen-modal > video {
+  float: right;
+  max-width: 40%;
+  max-height: 40%;
+}
+
+.fullscreen-modal {
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(8px);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  overflow: auto;
+}
+
+.fullscreen-modal > video {
+  position: absolute;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+
 .calling-buttons {
   position: absolute;
   bottom: 0;
@@ -58,7 +92,13 @@ stream.on("remove", () => {
   text-align: center;
 }
 
-.hangup {
+.calling-buttons > button {
+  background: #ffffff40;
+  backdrop-filter: blur(2px);
+  margin: 0 8px;
+}
+
+.calling-buttons > button.hangup {
   background-color: #df3d3d;
   color: white;
 }
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -86,16 +86,4 @@ async function openMediaStream() {
 .button-group span.mdi:active {
   color: deepskyblue;
 }
-
-.fullscreen-modal {
-  background-color: rgba(0, 0, 0, 0.5);
-  backdrop-filter: blur(8px);
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 100;
-  overflow: auto;
-}
 </style>
