a22f86fdeddf1aaf497102d7ead0ff7c72dac7f7
longern <771815005@qq.com>
2022-06-02 19:51:15 +0800

    Remove peerjs CDN

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,22 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
-import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    externalGlobals({ peerjs: "peerjs" }, { include: ["**/*.ts"] }),
-    vue(),
-    vueI18n(),
-  ],
+  plugins: [vue(), vueI18n()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          peerjs: ["peerjs"],
+        },
+      },
     },
   },
 });
